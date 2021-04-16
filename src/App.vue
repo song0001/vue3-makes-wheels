81599405d6f0433b84e360d242abd5b53c835f73
Spark <1751919770@qq.com>
2021-04-16 18:16:39 +0800

    创建vue3项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+ <Frank/>
+
+</template>
+
+<script>
+import Frank from './components/Frank.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Frank:Frank
+  }
+}
+</script>
