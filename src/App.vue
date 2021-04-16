5c0be9a092cc72681a3f01d905958acbf66fe6c6
Spark <1751919770@qq.com>
2021-04-16 18:57:36 +0800

    初始化vue-touter

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
- <Frank/>
+<div>导航栏|
+  <router-link to='/'>Frank</router-link>|
+    <router-link to='/frank2'>Frank2</router-link>
+</div>
+  <hr/>
+ <router-view/>
 
 </template>
 
 <script>
-import Frank from './components/Frank.vue'
+// import Frank from './components/Frank.vue'
 
 export default {
   name: 'App',
-  components: {
-    Frank:Frank
-  }
+  // components: {
+  //   Frank:Frank
+  // }
 }
 </script>
